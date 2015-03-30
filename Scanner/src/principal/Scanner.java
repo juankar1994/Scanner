65db0fef7cf64086b4f582b53149208d1c90ffdc
brandoncc94 <brandoncc94@gmail.com>
2015-03-30 14:47:58 -0600

    Finalización de la interfaz. Se agregan los archivos DatosArchivoController y ArchivoData

--- Scanner/src/principal/Scanner.java
+++ Scanner/src/principal/Scanner.java
@@ -1,15 +1,5 @@
 package principal;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author JuanCarlos
- */
 import java.io.File;
 
 public class Scanner {
